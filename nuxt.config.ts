970def8a3dabc7cc7aeb3c402ebd3714a1c662b3
Habib Habibi <67565736+Habibi-7@users.noreply.github.com>
2026-06-03 11:25:11 -0400

    added analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxtjs/i18n',
     '@nuxt/eslint',
+    '@vercel/analytics',
     'shadcn-nuxt',
   ],
   devtools: { enabled: true },
